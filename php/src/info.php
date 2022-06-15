bbef1bd8b1169114c8f0dce3079a552a5d3fffa0
ktsukadaira <107523407+ktsukadaira@users.noreply.github.com>
2022-06-15 13:25:26 +0900

    Update info.php

--- php/src/info.php
+++ php/src/info.php
@@ -6,3 +6,37 @@ echo $b;
 
 ?>
 a
+<html>
+<head><title>PHP TEST</title></head>
+<body>
+
+<?php
+
+$counter_file = 'counter.txt';
+$counter_lenght = 8;
+
+$fp = fopen($counter_file, 'r+');
+
+if ($fp){
+    if (flock($fp, LOCK_EX)){
+
+        $counter = fgets($fp, $counter_lenght);
+        $counter++;
+
+        rewind($fp);
+
+        if (fwrite($fp,  $counter) === FALSE){
+            print('ファイル書き込みに失敗しました');
+        }
+
+        flock($fp, LOCK_UN);
+    }
+}
+
+fclose($fp);
+
+print('count:'.$counter);
+
+?>
+</body>
+</html>
